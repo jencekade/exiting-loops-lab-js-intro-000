7797529a7d4950ffb635c40932c3337be7a4006e
jencekade <gaephuoc@gmail.com>
2018-02-03 10:14:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      break;
+    } else {
+      array[i] = changeValue;
+    }
+  }
+  return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let j = 0; j < array.length; j++) {
+    if (array[j] === skipValue) {
+      continue;
+    } else {
+      array[j] = changeValue;
+    }
+  }
+  return array;
+}
+
+function findBy(array, findFn) {
+  //let valueIs = findFn();
+  for (let k = 0; k < array.length; k++) {
+    //let valueIs = findFn(array[k])
+    if (findFn9array[k])) {
+      return array[k];
+    }
+  }
+  /*if (!array.includes(valueIs)) {
+    return null;
+  }*/
+  //return null;
+}
